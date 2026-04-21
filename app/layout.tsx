214d788d7c52f0cdc94a30a5d26c9277a4de2526
Tezo Gogaladze <tezogogaladze@gmail.com>
2026-04-22 02:30:27 +0400

    cache bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ka" className="h-full">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700;800&display=swap" rel="stylesheet" />
+      </head>
       <body
         className="min-h-full antialiased"
         style={{
